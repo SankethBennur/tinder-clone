066712de477da7451bf8c5808842315544666a73
SankethBennur <snkthbnnr3@gmail.com>
2021-12-03 20:00:41 +0530

    changes

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const axiosInstance = axios.create({
-     baseURL: "https://localhost:5000/"  // should be baseURL
+     baseURL: "http://localhost:5000/"  // should be baseURL
 });
 
 module.exports = axiosInstance;
