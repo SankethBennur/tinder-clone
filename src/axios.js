78b9054441f582d4e3cea013745b6da3ba09682c
SankethBennur <snkthbnnr3@gmail.com>
2021-12-05 22:13:24 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const axiosInstance = axios.create({
-     baseURL: "http://localhost:5000/"  // should be baseURL
+     baseURL: "https://tinder-clone-2509.herokuapp.com/"  // should be baseURL
 });
 
 module.exports = axiosInstance;
