5a8799bccc808292e90fa389000c5f305e4b4deb
SankethBennur <snkthbnnr3@gmail.com>
2021-12-15 16:58:40 +0530

    connecting to mongodb atlas with axios

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,8 @@ const axios = require('axios');
 
 const axiosInstance = axios.create({
      // baseURL: "https://tinder-clone-2509.herokuapp.com/"  // should be baseURL
-     baseURL: "http://localhost:5000/"  // should be baseURL
+     // baseURL: "http://localhost:5000/"  // should be baseURL
+     baseURL: "mongodb+srv://tinder-admin:tinder-admin@tinderdb-cluster.yyngb.mongodb.net/tinderdb?retryWrites=true&w=majority"  // should be baseURL
 });
 
 module.exports = axiosInstance;
